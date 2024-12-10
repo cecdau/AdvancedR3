efd88d96a102146f1d188e2222ac634bc3e7f4bd
Cecilie DO <cecilie.daugaard@gmail.com>
2024-12-10 16:27:39 +0100

    Making function summary statistics

---
+++ R/functions.R
@@ -0,0 +1,22 @@
+
+#' Funcion for summary statistics
+#'
+#' @param data
+#'
+#' @return A data.frame/tibble
+
+descriptive_stats <- function(data) {
+    data %>%
+        dplyr::group_by(metabolite) %>%
+        dplyr::summarise(dplyr::across(
+            value,
+            base::list(
+                average = mean,
+                sd = sd
+            )
+        )) %>%
+        dplyr::mutate(dplyr::across(
+            dplyr::where(is.numeric),
+            ~ base::round(.x, digits = 1)
+        ))
+}
