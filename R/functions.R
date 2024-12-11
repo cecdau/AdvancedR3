0afe54751036ae808bcfc2c76b146ad7c5e378a2
Cecilie DO <cecilie.daugaard@gmail.com>
2024-12-11 10:00:13 +0100

    function with plots

--- R/functions.R
+++ R/functions.R
@@ -20,3 +20,17 @@ descriptive_stats <- function(data) {
             ~ base::round(.x, digits = 1)
         ))
 }
+
+
+#' A plot of data
+#'
+#' @param data
+#'
+#' @return A plot object
+
+plot_distributions <- function(data) {
+    ggplot2::ggplot(data, aes(x = value)) +
+        ggplot2::geom_histogram() +
+        ggplot2::facet_wrap(vars(metabolite), scales = "free")
+}
+
