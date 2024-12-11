d96239031b85d2bc0be7f5d7c9a232f22e7fbf02
Cecilie DO <cecilie.daugaard@gmail.com>
2024-12-11 10:22:29 +0100

    add target quarto

--- R/functions.R
+++ R/functions.R
@@ -29,8 +29,8 @@ descriptive_stats <- function(data) {
 #' @return A plot object
 
 plot_distributions <- function(data) {
-    ggplot2::ggplot(data, aes(x = value)) +
+    ggplot2::ggplot(data, ggplot2::aes(x = value)) +
         ggplot2::geom_histogram() +
-        ggplot2::facet_wrap(vars(metabolite), scales = "free")
+        ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
 }
 
